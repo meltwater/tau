7a7bbed2f83ba8a4e931fde0bd7b44371639dd14
Evan Sosenko <evan.sosenko@meltwater.com>
2017-08-28 16:50:59 -0700

    Move publish step filter to circleci config

--- .circleci/publish.sh
+++ .circleci/publish.sh
@@ -3,18 +3,18 @@
 set -e
 set -u
 
-if [[ "${CIRCLE_BRANCH}" == 'master' || "${CIRCLE_BRANCH}" =~ ^v[0-9] ]]; then
-  apt-get update
-  apt-get install -y jq
-  pkg_name=$(jq -r '.name' package.json)
-  pkg_version=$(jq -r '.version' package.json)
-  if [[ "$(git log -1 --pretty='%s')" == "${pkg_version}" ]]; then
-    if [[ -z "$(npm view ${pkg_name}@${pkg_version})" ]]; then
-      pkg_files=(*.tgz)
-      npm publish $(echo $pkg_files)
-      if [[ -n "${NPM_TEAM:-}" ]]; then
-        npm access grant read-only $NPM_TEAM
-      fi
+apt-get update
+apt-get install -y jq
+
+pkg_name=$(jq -r '.name' package.json)
+pkg_version=$(jq -r '.version' package.json)
+
+if [[ "$(git log -1 --pretty='%s')" == "${pkg_version}" ]]; then
+  if [[ -z "$(npm view ${pkg_name}@${pkg_version})" ]]; then
+    pkg_files=(*.tgz)
+    npm publish $(echo $pkg_files)
+    if [[ -n "${NPM_TEAM:-}" ]]; then
+      npm access grant read-only $NPM_TEAM
     fi
   fi
 fi
