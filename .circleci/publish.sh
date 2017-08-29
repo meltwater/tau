5338fac65ec330f1f727eaf9b655910d1e3047c8
Evan Sosenko <evan.sosenko@meltwater.com>
2017-08-28 17:00:51 -0700

    Add better logging to publish.sh

--- .circleci/publish.sh
+++ .circleci/publish.sh
@@ -13,8 +13,25 @@ if [[ "$(git log -1 --pretty='%s')" == "${pkg_version}" ]]; then
   if [[ -z "$(npm view ${pkg_name}@${pkg_version})" ]]; then
     pkg_files=(*.tgz)
     npm publish $(echo $pkg_files)
-    if [[ -n "${NPM_TEAM:-}" ]]; then
-      npm access grant read-only $NPM_TEAM
-    fi
+    echo
+    echo "> Published ${pkg_name}@${pkg_version}."
+    echo
+  else
+    echo
+    echo "> Already published ${pkg_name}@${pkg_version}."
+    echo
+    exit
   fi
+else
+  echo
+  echo '> Nothing to publish: not a new version commit.'
+  echo
+  exit
+fi
+
+if [[ -n "$(npm view ${pkg_name})" && -n "${NPM_TEAM:-}" ]]; then
+  npm access grant read-only $NPM_TEAM
+  echo
+  echo "> Access granted to npm team ${NPM_TEAM}."
+  echo
 fi
