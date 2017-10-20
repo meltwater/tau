09b4d7c5f65fa391f493f53240563e08e784b9e5
Evan Sosenko <evan.sosenko@meltwater.com>
2017-10-20 15:40:43 -0700

    Move example running code to @meltwater/examplr

--- examples/index.js
+++ examples/index.js
@@ -1,10 +1,8 @@
 import 'source-map-support/register'
 
-import fs from 'fs'
 import path from 'path'
 
-import { createLogger as createBunyan, stdSerializers } from 'bunyan'
-import { camelCase, paramCase } from 'change-case'
+import createExamples from '@meltwater/examplr'
 
 import isTrue from './is-true' // TODO: Replace this with added example.
 
@@ -18,67 +16,15 @@ const envVars = [
 
 const defaultOptions = {}
 
-const envOptions = env => Object.assign.apply({}, [{},
-  ...envVars.filter(k => env[k] !== undefined)
-    .map(k => ({[camelCase(k)]: env[k]}))]
-)
-
-const localOptions = local => (
-  fs.existsSync(local)
-    ? Object.assign({}, defaultOptions, JSON.parse(fs.readFileSync(local)))
-    : defaultOptions
-)
-
-const createLogger = ({serializers, ...options}) => createBunyan({
-  serializers: {err: stdSerializers.err, ...serializers},
-  ...options
+const { createExample, runExample } = createExamples({
+  examples,
+  envVars,
+  defaultOptions
 })
 
-const createExample = (name, {
-  log = createLogger({name}),
-  ...options
-} = {}) => async (...args) => {
-  try {
-    if (!name) throw new Error('Must specify example name as first argument.')
-
-    const example = examples[camelCase(name)]
-
-    if (typeof example !== 'function') {
-      throw new Error(`Example ${name} not found.`)
-    }
-
-    log.info('Example: Start')
-    const data = await example({...options, log})(...args)
-    log.info({data}, 'Example: Success')
-    return data
-  } catch (err) {
-    log.error({err}, 'Example: Fail')
-    throw err
-  }
-}
-
 if (require.main === module) {
-  const local = path.resolve(__dirname, 'local.json')
-  const { name } = JSON.parse(fs.readFileSync('package.json'))
-  const example = process.argv.slice(2)[0]
-  const args = process.argv.slice(3)
-  const options = {...localOptions(local), ...envOptions(process.env)}
-  const level = options.logLevel || 'info'
-  const log = createLogger({name, example, level})
-
-  if (!example) {
-    console.log()
-    console.log('Runnable examples:')
-    Object.keys(examples).sort().forEach(e => {
-      console.log('  ', paramCase(e))
-    })
-    console.log()
-    process.exit(0)
-  }
-
-  createExample(example, {...options, log})(...args).catch(() => {
-    log.fatal('Example: Fatal')
-    process.exit(1)
+  runExample({
+    local: path.resolve(__dirname, 'local.json')
   })
 }
 
