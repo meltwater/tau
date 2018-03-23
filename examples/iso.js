bac899813dc74e733a1cfa42d60f652292a1267d
Evan Sosenko <evan.sosenko@meltwater.com>
2018-03-23 12:34:08 -0700

    Fix Settings set functions

--- examples/iso.js
+++ examples/iso.js
@@ -1,9 +1,8 @@
-import { Settings } from 'luxon'
 import { compose } from 'ramda'
 
-import { day, fromIso } from '../lib'
+import { setThrowOnInvalid, day, fromIso } from '../lib'
 
 export default ({log}) => async (iso = new Date().toISOString()) => {
-  Settings.throwOnInvalid = true
+  setThrowOnInvalid(true)
   return compose(day, fromIso)(iso)
 }
