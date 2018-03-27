5e279e55a268c912f4c0ad7139155306a6b83f10
Evan Sosenko <evan.sosenko@meltwater.com>
2018-03-26 17:34:17 -0700

    Export now

--- examples/iso.js
+++ examples/iso.js
@@ -1,8 +1,8 @@
 import { compose } from 'ramda'
 
-import { setThrowOnInvalid, day, fromIso } from '../lib'
+import { setThrowOnInvalid, day, fromFormatWithOptions, withZone } from '../lib'
 
 export default ({log}) => async (iso = new Date().toISOString()) => {
   setThrowOnInvalid(true)
-  return compose(day, fromIso)(iso)
+  return compose(day, withZone('utc')(fromFormatWithOptions)('YYYY'))(iso)
 }
