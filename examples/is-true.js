34a00a8b55cd588267d808f330967c5cc4da5cd4
Evan Sosenko <evan.sosenko@meltwater.com>
2018-10-01 16:42:49 -0700

    Remove non-doc files

--- examples/is-true.js
+++
@@ -1,8 +0,0 @@
-import { isTrue } from '../lib'
-
-export default ({log}) => async (check = true) => {
-  log.debug({check}, 'Input')
-  const result = await isTrue(check)
-  if (!result) throw new Error('Check was not true.')
-  return result
-}
--- index.js
+++
@@ -1,2 +0,0 @@
-export * from 'date-fns/fp'
-export * from './lib'
