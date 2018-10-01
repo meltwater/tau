84c609104f6e8892a87767be89245564e3a26cf0
Evan Sosenko <evan.sosenko@meltwater.com>
2018-10-01 16:29:04 -0700

    Format for Standard 12

--- examples/iso.js
+++ examples/iso.js
@@ -2,7 +2,7 @@ import { compose } from 'ramda'
 
 import { setThrowOnInvalid, day, fromFormatWithOptions, withZone } from '../lib'
 
-export default ({log}) => async (iso = new Date().toISOString()) => {
+export default ({ log }) => async (iso = new Date().toISOString()) => {
   setThrowOnInvalid(true)
   return compose(day, withZone('utc')(fromFormatWithOptions)('YYYY'))(iso)
 }
