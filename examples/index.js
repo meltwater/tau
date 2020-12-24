a4cc9be98638773e51980c41461c233173a80eb2
Evan Sosenko <evan.sosenko@meltwater.com>
2020-12-24 11:22:40 -0600

    Update to examplr v6

--- examples/index.js
+++ examples/index.js
@@ -1,30 +1,23 @@
 import path from 'path'
 
-import createExamples from '@meltwater/examplr'
+import { createExamples } from '@meltwater/examplr'
 
-import iso from './iso'
+import iso from './iso.js'
 
 export const examples = {
   iso
 }
 
-// prettier-ignore
-const envVars = [
-  'LOG_LEVEL',
-  'LOG_FILTER',
-  'LOG_OUTPUT_MODE'
-]
+const envVars = ['LOG_LEVEL', 'LOG_FILTER', 'LOG_OUTPUT_MODE']
 
 const defaultOptions = {}
 
-if (require.main === module) {
-  const { runExample } = createExamples({
-    examples,
-    envVars,
-    defaultOptions
-  })
+const { runExample } = createExamples({
+  examples,
+  envVars,
+  defaultOptions
+})
 
-  runExample({
-    local: path.resolve(__dirname, 'local.json')
-  })
-}
+runExample({
+  local: path.resolve('examples', 'local.json')
+})
