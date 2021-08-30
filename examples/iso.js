bda43f3858e73d3dbed16c8301ebe0bf23fa5564
Evan Sosenko <razorx@evansosenko.com>
2021-08-30 11:42:00 -0700

    Run format

--- examples/iso.js
+++ examples/iso.js
@@ -2,7 +2,8 @@ import { compose } from 'ramda'
 
 import { setThrowOnInvalid, day, fromIso } from '../index.js'
 
-export default ({ log }) => async (iso = new Date().toISOString()) => {
-  setThrowOnInvalid(true)
-  return compose(day, fromIso)(iso)
-}
+export default ({ log }) =>
+  async (iso = new Date().toISOString()) => {
+    setThrowOnInvalid(true)
+    return compose(day, fromIso)(iso)
+  }
